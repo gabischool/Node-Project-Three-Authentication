43a9fec0b8bc6a586cd9e71fff613b72c1a0fd9c
Miirshe <miirshe@gmail.com>
2023-10-19 14:04:34 +0300

    currently working owner and author in a backendside

---
+++ api/routes/owner_routes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { delete_owner, get_owner, get_owners, login_owner, register_owner, update_owner } from '../controller/controller_owner.js';
+import { authenticate_owner } from '../middleware/authenticate_owner.js';
+export const owner_routes = express.Router();
+owner_routes.post('/owner/register', register_owner)
+owner_routes.post('/owner/login', login_owner)
+owner_routes.get('/owners', get_owners)
+owner_routes.get('/current/owner', authenticate_owner, get_owner)
+owner_routes.delete('/owner/:id', delete_owner)
+owner_routes.put('/owner/:id', update_owner)
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import { author_routes } from './api/routes/author_routes.js'
+import { owner_routes } from './api/routes/owner_routes.js';
 
 const PORT = 9000;
 const app = express()
@@ -10,6 +11,7 @@ app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
 app.use('/api', author_routes)
+app.use('/api', owner_routes)
 
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
