d3232947e619843896c3c24a95531cb4f5df7e33
Miirshe <miirshe@gmail.com>
2023-10-19 12:15:19 +0300

    currently working backend side

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Import server here and start the application
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import { author_routes } from './api/routes/author_routes.js'
+
+const PORT = 9000;
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+app.use('/api', author_routes)
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+})
